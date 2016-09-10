20dc41087a1265f1c3689f696fe81bac20df501f
maoli8087 <18811129930@163.com>
2016-09-10 15:36:14 +0800

    Maoli: 修改样式和轮播图

---
+++ js/swiper.js
@@ -0,0 +1,23 @@
+window.onload=function(){
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        autoplay:1000,
+        speed:2000,
+        direction: 'horizontal',
+//            direction: 'vertical',
+        loop: true,
+
+        // 如果需要分页器
+        pagination: '.swiper-pagination'
+
+        // 如果需要前进后退按钮
+//            nextButton: '.swiper-button-next',
+//            prevButton: '.swiper-button-prev',
+
+        // 如果需要滚动条
+//            scrollbar: '.swiper-scrollbar',
+
+
+    })
+}
+
